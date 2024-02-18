3f563049373626e29c577a49221a88ede1d8cb6b
hawardShin <joushin@student.42seoul.kr>
2024-02-18 21:57:57 +0900

    Feat: Lombok Library

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,37 +9,14 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
      private final MemberRepository memberRepository;
      private final DiscountPolicy discountPolicy ;
 
-
-
-    // ///수정자 주입
-    // @Autowired
-    // public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-    //     // System.out.println("discountPolicy = " + discountPolicy);
-    //     this.discountPolicy = discountPolicy;
-    // }
-    // @Autowired(required = false)
-    // public void setMemberRepository(MemberRepository memberRepository) {
-    //     // System.out.println("memberRepository = " + memberRepository);
-    //     this.memberRepository = memberRepository;
-    // }
-    // //생성자 주입
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        // System.out.println("memberRepository = " + memberRepository + ", discountPolicy = " + discountPolicy);
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    // @Autowired
-    // public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    //     this.memberRepository = memberRepository;
-    //     this.discountPolicy = discountPolicy;
-    // }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
