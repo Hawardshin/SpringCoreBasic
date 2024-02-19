b8a079006a537e869c13d6b619ce923990be4174
hawardShin <joushin@student.42seoul.kr>
2024-02-19 17:18:27 +0900

    Feat: Use primary without Qualifier

--- src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -7,7 +7,6 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 
 @Component
-@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy{
     private int discountFixAmount = 1000;// 1000원 할인
     
